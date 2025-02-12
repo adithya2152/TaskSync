f6a07789616e82febf2d9272fc8fbde201c765df
adithya2152 <adithyabharadwaj15@gmail.com>
2025-02-12 12:37:14 +0530

    route-fix

--- src/app/home/(handlers)/updateTask/[tid]/route.ts
+++ src/app/home/(handlers)/updateTask/[tid]/route.ts
@@ -2,19 +2,28 @@ import { NextResponse } from "next/server";
 import { updateTask } from "@/db";
 import type { NextRequest } from "next/server";
 
-export async function PUT(request: NextRequest, context: { params: { tid: string } }) {
-    try {
-        const { tid } = context.params; // Access params correctly
-        const taskId = parseInt(tid, 10);
 
-        if (isNaN(taskId)) {
+
+export async function PUT(request: NextRequest)
+{
+    try{
+
+        const{pathname} = request.nextUrl
+        const id = pathname.split('/').pop()
+
+        console.log("param ID" , id);
+
+        if(!id)
+        {
             return new NextResponse(JSON.stringify({ error: "Invalid task ID" }), { status: 400 });
         }
 
-        await updateTask(taskId);
+        await updateTask(parseInt(id));
         return new NextResponse(JSON.stringify({ message: "Task updated successfully" }), { status: 200 });
+    }
+    catch(error)
+    {
 
-    } catch (error) {
         console.error("Error updating task:", error);
         return new NextResponse(
             JSON.stringify({ error: "Failed to update task" }),
